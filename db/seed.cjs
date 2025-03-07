f2d2ffdf676e85ac39684503d72ec4ada541f442
carolyn sulzen <carolynsulzen@carolyns-MacBook-Pro.local>
2025-03-06 21:47:29 -0700

    add users, appliances, kitchen_appliances_carts, and carts tables

--- db/seed.cjs
+++ db/seed.cjs
@@ -3,8 +3,10 @@ const client = require('./client.cjs');
 const dropTables = async () =>{
   try{
     await client.query(`
+      DROP TABLE IF EXISTS kitchen_appliances_carts;
+      DROP TABLE IF EXISTS carts;
       DROP TABLE IF EXISTS users;
-      DROP TABLE IF EXISTS appliances;
+      DROP TABLE IF EXISTS kitchen_appliances;
       `);
   }catch(err){
     console.log(err)
@@ -19,13 +21,23 @@ const createTables = async () =>{
       username VARCHAR(30) UNIQUE NOT NULL,
       password VARCHAR(60) NOT NULL);
       
-      CREATE TABLE appliances(
+      CREATE TABLE kitchen_appliances(
       id SERIAL PRIMARY KEY,
       name VARCHAR(50) NOT NULL,
       type VARCHAR(30) NOT NULL,
       color VARCHAR(30) NOT NULL,
       rating CHAR(1),
       availability BOOLEAN);
+
+      CREATE TABLE carts (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(id),
+      is_open BOOLEAN);
+
+      CREATE TABLE kitchen_appliances_carts(
+      id SERIAL PRIMARY KEY,
+      kitchen_appliances_id INTEGER REFERENCES kitchen_appliances(id),
+      carts_id INTEGER REFERENCES carts(id));
       `);
 
     
